c0ec5f88febae61f0a46f4697da18fc7d2e20994
SuperMido <supermido1996@gmail.com>
2021-05-12 09:41:37 +0700

    Add test and fix bug

--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -6,7 +6,7 @@ contract CampaignFactory {
     function createCampaign(uint minimum) public {
         address newCampaign = new Campaign(minimum, msg.sender);
         deployedCampaigns.push(newCampaign);
-    } 
+    }
 
     function getDeployedCampaigns() public view returns (address[]) {
         return deployedCampaigns;
@@ -44,5 +44,53 @@ contract Campaign {
 
         approvers[msg.sender] = true;
         approversCount++;
+    }
+
+    function createRequest(string description, uint value, address recipient) public restricted {
+        Request memory newRequest = Request({
+           description: description,
+           value: value,
+           recipient: recipient,
+           complete: false,
+           approvalCount: 0
+        });
+
+        requests.push(newRequest);
+    }
+
+    function approveRequest(uint index) public {
+        Request storage request = requests[index];
+
+        require(approvers[msg.sender]);
+        require(!request.approvals[msg.sender]);
+
+        request.approvals[msg.sender] = true;
+        request.approvalCount++;
+    }
+
+    function finalizeRequest(uint index) public restricted {
+        Request storage request = requests[index];
+
+        require(request.approvalCount > (approversCount / 2));
+        require(!request.complete);
+
+        request.recipient.transfer(request.value);
+        request.complete = true;
+    }
+
+    function getSummary() public view returns (
+      uint, uint, uint, uint, address
+      ) {
+        return (
+          minimumContribution,
+          this.balance,
+          requests.length,
+          approversCount,
+          manager
+        );
+    }
+
+    function getRequestsCount() public view returns (uint) {
+        return requests.length;
     }
 }
