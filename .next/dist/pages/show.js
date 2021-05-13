48756483e5229de00f01731a3f711494a0a6d57c
SuperMido <supermido1996@gmail.com>
2021-05-13 11:21:37 +0700

    Implement react nextjs

---
+++ .next/dist/pages/show.js
@@ -0,0 +1,23 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = 'D:\\Desktop\\kickstart\\pages\\show.js?entry';
+
+exports.default = function () {
+    return _react2.default.createElement('h1', {
+        __source: {
+            fileName: _jsxFileName,
+            lineNumber: 5
+        }
+    }, 'Welcome to the show page!!!');
+};
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzXFxzaG93LmpzIl0sIm5hbWVzIjpbIlJlYWN0Il0sIm1hcHBpbmdzIjoiOzs7Ozs7QUFBQSxBQUFPLEFBRVA7Ozs7Ozs7O2tCQUFlLFlBQU0sQUFDakI7MkJBQ0ksY0FBQTs7c0JBQUE7d0JBQUE7QUFBQTtBQUFBLEtBQUEsRUFESixBQUNJLEFBRVA7QUFKRCIsImZpbGUiOiJzaG93LmpzP2VudHJ5Iiwic291cmNlUm9vdCI6IkQ6L0Rlc2t0b3Ava2lja3N0YXJ0In0=
