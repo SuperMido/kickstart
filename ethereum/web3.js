398a20500d519974f0986c021eacaf887cc63703
SuperMido <supermido1996@gmail.com>
2021-05-13 15:23:27 +0700

    fix windows provider

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,5 +1,15 @@
 import Web3 from 'web3';
 
-const web3 = new Web3(window.web3.currentProvider);
+let web3;
+
+if (typeof windows !== 'undefined' && windows.web3 !== 'undefined') {
+    web3 = new Web3(windows.web3.currentProvider)
+} else {
+    const provider = new Web3.providers.HttpProvider(
+        'https://rinkeby.infura.io/v3/d7dde0fa90274c6d873d75e84505f624'
+    );
+
+    web3 = new Web3(provider);
+}
 
 export default web3;
