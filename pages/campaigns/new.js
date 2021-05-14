6bfbf0460c3b19c56af96dc030489735cc14696e
SuperMido <supermido1996@gmail.com>
2021-05-14 16:51:43 +0700

    Button spinner

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -7,11 +7,14 @@ import web3 from '../../ethereum/web3';
 class CampaignNew extends Component {
     state = {
         minimumContribution: '',
-        errorMessage: ''
+        errorMessage: '',
+        loading: false
     };
 
     onSubmit = async event => {
         event.preventDefault();
+
+        this.setState({ loading: true });
 
         try {
             const accounts = await web3.eth.getAccounts();
@@ -23,6 +26,8 @@ class CampaignNew extends Component {
         } catch (error) {
             this.setState({ errorMessage: error.message })
         }
+
+        this.setState({ loading: false });
     };
 
     render() {
@@ -41,7 +46,7 @@ class CampaignNew extends Component {
                         />
                     </Form.Field>
                     <Message error header="Oops!" content={this.state.errorMessage} />
-                    <Button primary>
+                    <Button loading={this.state.loading} primary>
                         Create!
                     </Button>
                 </Form>
