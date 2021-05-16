d96278ab118dc92d7b82f144d26250a64c327a40
SuperMido <supermido1996@gmail.com>
2021-05-16 13:32:53 +0700

    Error message and spnning

--- components/ContributeForm.js
+++ components/ContributeForm.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import { Button, Form, FormInput, Input } from 'semantic-ui-react';
+import { Button, Form, FormInput, Input, Message } from 'semantic-ui-react';
 import Campaign from '../ethereum/campaign';
 import web3 from '../ethereum/web3';
 import  { Router } from '../routes';
 
 class ContributionForm extends Component {
     state = {
-        value: ''
+        value: '',
+        errorMessage: '',
+        loading: false
     };
 
     onSubmit = async (event) => {
         event.preventDefault();
         
         const campaign = Campaign(this.props.address);
+
+        this.setState({ loading: true , errorMessage: ''});
 
         try {
             const accounts = await web3.eth.getAccounts()
@@ -23,13 +27,15 @@ class ContributionForm extends Component {
 
             Router.replaceRoute(`/campaigns/${this.props.address}`);
         } catch (error) {
-            
+            this.setState({ errorMessage: error.message });
         }
+
+        this.setState({ loading: false, value: '' });
     };
 
     render () {
         return (
-            <Form onSubmit={this.onSubmit}>
+            <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
             <Form.Field>
                 <label>Amount to Contribute</label>
                 <Input 
@@ -39,7 +45,9 @@ class ContributionForm extends Component {
                     labelPosition="right"
                 />
             </Form.Field>
-            <Button primary>
+
+            <Message error header="Oops!" content={this.state.errorMessage} />
+            <Button primary loading={this.state.loading}>
                 Contribute!
             </Button>
         </Form>
